0d951c92aaa5ac3b14c014b0773476efeecdbd22
Idoko Favour <ojodomofavour2004@gmail.com>
2023-10-04 19:41:28 +0100

    commit all files

---
+++ script.js
@@ -0,0 +1,56 @@
+function fetchUser() {
+    showSpiner();
+    fetch('https://randomuser.me/api')
+        .then((response) => response.json())
+        .then((data) => {
+            hideSpiner()
+            displayUser(data.results[0]);
+        })
+}
+
+function displayUser(user) {
+    const userDisplay = document.querySelector('#user')
+    if(user.gender === 'female' ) {
+        document.body.style.backgroundColor = 'rebeccapurple';
+    } else {
+        document.body.style.backgroundColor = 'steelblue';
+    }
+
+    userDisplay.innerHTML = `
+        <div class="flex justify-betweem">
+        <div class="flex">
+            <img class="w-48 h-48 rounded-full mr-8"
+            src="${user.picture.large}" 
+            />
+            <div class="space-y-3">
+                <p class="text-xl">
+                    <span class="font-bold">Name:</span>${user.name.first} ${user.name.last}
+                </p>
+                <p class="text-xl">
+                    <span class="font-bold">Email:</span>${user.email}
+                </p>
+                <p class="text-xl">
+                    <span class="font-bold">Phone:</span>5) ${user.phone}
+                </p>
+                <p class="text-xl">
+                    <span class="font-bold">Name:</span> ${user.location.city} ${user.location.country}
+                </p>
+                <p class="text-xl">
+                    <span class="font-bold">Age:</span>${user.dob.age}
+                </p>
+            </div>
+        </div>
+    </div>
+    `
+}
+
+function showSpiner() {
+    document.querySelector('.spinner').style.display = 'block';
+}
+function hideSpiner() {
+    document.querySelector('.spinner').style.display = 'none';
+}
+
+document.querySelector('#generate').addEventListener('click', fetchUser)
+
+ fetchUser();
